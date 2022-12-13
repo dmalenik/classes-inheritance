055bc655887341114d971a88f63f740a2463a866
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-13 21:59:31 +0100

    feat(StringBuilder): set divide method for StringBuilder prototype

--- StringBuilder.mjs
+++ StringBuilder.mjs
@@ -18,13 +18,18 @@ StringBuilder.prototype.minus = function (n) {
   return this;
 };
 StringBuilder.prototype.multiply = function (n) {
-  this.str = Array
-    .from({ length: n }, () => [this.str])
-    .join(' ');
+  this.str = Array.from({ length: n }, () => [this.str]).join(" ");
+
+  return this;
+};
+StringBuilder.prototype.divide = function (n) {
+  let k = Math.floor(this.str.length / n);
+
+  this.str = this.str.slice(0, k);
 
   return this;
 };
 
 let strBuilder = new StringBuilder("Hello");
 
-console.log(strBuilder.plus(" all", "!").minus(4).multiply(3));
+console.log(strBuilder.plus(" all", "!").minus(4).multiply(3).divide(4));
