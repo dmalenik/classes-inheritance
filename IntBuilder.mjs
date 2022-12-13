757f7a760f06fccce1e37ab14a3ece7458924f57
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-13 17:56:01 +0100

    feat(IntBuilder): add plus method

--- IntBuilder.mjs
+++ IntBuilder.mjs
@@ -1,11 +1,18 @@
 import { Base } from "./base-class.mjs";
 
 class IntBuilder extends Base {
-  constructor() {
+  constructor(int = 0) {
     super();
+    this.int = int;
+  }
+
+  plus(...n) {
+    return n.reduce((prev, curr) => prev + curr, this.int);
   }
 }
 
-let int = new IntBuilder();
+let intBuilder = new IntBuilder(10);
 
-console.log(int.sayHello());
+console.log(
+  intBuilder.plus(2, 3, 2)
+);
