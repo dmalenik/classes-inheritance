1435c36dd64602151f26675a0a7493845e78ea2a
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-13 19:59:43 +0100

    feat(StringBuilder): add inheritance to StringBuilder from Base

--- StringBuilder.mjs
+++ StringBuilder.mjs
@@ -1,6 +1,11 @@
 import { Base } from "./Base.mjs";
 
-function StringBuilder() {}
+function StringBuilder(str = "") {
+  this.str = str;
+}
 
 StringBuilder.prototype = Object.create(Base.prototype);
 StringBuilder.prototype.constructor = StringBuilder;
+
+let s = new StringBuilder("Hello!");
+
