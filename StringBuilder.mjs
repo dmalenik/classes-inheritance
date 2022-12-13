60cd32ce7090adf5642af7d9b05b522af0fc358d
Dmitriy Malenik <dmalenik@gmail.com>
2022-12-13 20:23:55 +0100

    feat(StringBuilder): set a method chain for StringBuilder instance

--- StringBuilder.mjs
+++ StringBuilder.mjs
@@ -7,12 +7,17 @@ function StringBuilder(str = "") {
 StringBuilder.prototype = Object.create(Base.prototype);
 StringBuilder.prototype.constructor = StringBuilder;
 
-StringBuilder.prototype.plus = function (...str) { 
+StringBuilder.prototype.plus = function (...str) {
   this.str = str.reduce((prev, curr) => prev + curr, this.str);
+
+  return this;
+};
+StringBuilder.prototype.minus = function (n) {
+  this.str = this.str.slice(0, -n);
 
   return this;
 };
 
 let strBuilder = new StringBuilder("Hello");
 
-console.log(strBuilder.plus(' all', '!'));
+console.log(strBuilder.plus(" all", "!").minus(4));
